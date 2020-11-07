475859b7c1fa196bfc0c27c7e8f546e40c4fc1e6
VuVanHoc <hocvanvu1999@gmail.com>
2020-11-07 23:03:21 +0700

    init project

---
+++ src/main/java/com/uet/hocvv/equiz/EquizApplication.java
@@ -0,0 +1,13 @@
+package com.uet.hocvv.equiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquizApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EquizApplication.class, args);
+	}
+	
+}
