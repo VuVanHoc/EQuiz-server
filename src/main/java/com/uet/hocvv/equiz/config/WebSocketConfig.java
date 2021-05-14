e94456bdd96a30fdedd093a2df987c6394db6090
VuVanHoc <hocvanvu1999@gmail.com>
2021-05-14 15:44:06 +0700

    fix cors

--- src/main/java/com/uet/hocvv/equiz/config/WebSocketConfig.java
+++ src/main/java/com/uet/hocvv/equiz/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.uet.hocvv.equiz.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
 		stompEndpointRegistry.addEndpoint("/ws")
-				.setAllowedOrigins("http://localhost:9000", "https://equiz-uet.netlify.app/")
+				.setAllowedOrigins("*")
 				.withSockJS();
 	}
 
